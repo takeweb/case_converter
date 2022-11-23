9cc08576dbd8be46b87e2dcf89d0f33a8a7de924
Taketomo Ohishi <takeweb@mac.com>
2022-11-23 20:54:47 +0900

    add kebab case

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Case Converter
 
-* convert snake case to camel case
-* convert camel case to snake case
+* Convert snake case to camel case
+* Convert kebab case to camel case
+* Convert camel case to snake case
+* Convert camel case to kebab case
+* Convert kebab case to snake case
+* Convert snake case to kebab case
 
 ## Installation
 
@@ -11,7 +15,7 @@ cargo add case_converter
 
 ## Example for Convert snake case to camel case
 ```
-use case_converter;
+use case_converter::snake_to_camel;
 
 fn main() {
     let result = snake_to_camel("snake_to_camel");
@@ -19,12 +23,52 @@ fn main() {
 }
 ```
 
+## Example for Convert kebab case to camel case
+```
+use case_converter::kebab_to_camel;
+
+fn main() {
+    let result = kebab_to_camel("kebab-to-camel");
+    println!("{}", result); // → kebabToCamel
+}
+```
+
 ## Example for Convert camel case to snake case
 ```
-use case_converter;
+use case_converter::camel_to_snake;
 
 fn main() {
     let result = camel_to_snake("camelToSnake");
     println!("{}", result); // → camel_to_snake
+}
+```
+
+## Example for Convert camel case to kebab case
+```
+use case_converter::camel_to_kebab;
+
+fn main() {
+    let result = camel_to_kebab("camelToKebab");
+    println!("{}", result); // → camel-to-kebab
+}
+```
+
+## Example for Convert kebab case to snake case
+```
+use case_converter::kebab_to_snake;
+
+fn main() {
+    let result = kebab_to_snake("kebab-to-snake");
+    println!("{}", result); // → kebab_to_snake
+}
+```
+
+## Example for Convert snake case to kebab case
+```
+use case_converter::snake_to_kebab;
+
+fn main() {
+    let result = snake_to_kebab("snake_to_kebab");
+    println!("{}", result); // → snake-to-kebab
 }
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,28 @@ pub fn snake_to_camel(arg_str: &str) -> String {
         .collect()
 }
 
-// 文字列単位の処理(snake_to_camel用)
+/// Convert kebab case to camel case
+/// # Example
+/// ```
+/// use case_converter::kebab_to_camel;
+///
+/// let result = kebab_to_camel("kebab-to-camel");
+/// println!("{}", result); // → kebabToCamel
+/// ```
+pub fn kebab_to_camel(arg_str: &str) -> String {
+    // 「-(ハイフン)」で文字列部を分割
+    let list: Vec<&str> = arg_str
+                            .split('-')
+                            .map(str::trim)
+                            .collect();
+    // 分割した文字列を処理
+    list.into_iter()
+        .enumerate()
+        .map(|(i, val)| make_camel(i, val))
+        .collect()
+}
+
+// 文字単位のキャメルケース変換処理
 fn make_camel(i: usize, s: &str) -> String {
     if i == 0 {
         String::from(s)
@@ -33,7 +54,7 @@ fn make_camel(i: usize, s: &str) -> String {
     }
 }
 
-// 文字単位の処理(snake_to_camel用)
+// 文字単位の小文字変換処理
 fn to_uppercase(i: usize, c: &char) -> char {
     if i == 0 {
         c.to_ascii_uppercase()
@@ -50,24 +71,62 @@ fn to_uppercase(i: usize, c: &char) -> char {
 /// let result = camel_to_snake("camelToSnake");
 ///  println!("{}", result); // → camel_to_snake
 /// ```
-// キャメルケース文字列をスネークケース文字列へ変換
 pub fn camel_to_snake(arg_str: &str) -> String {
     let chars: Vec<char> = arg_str.chars().collect();
     chars.into_iter()
-        .map(|c| to_lowercase(c))
+        .map(|c| to_lowercase(c, '_'))
         .collect()
 }
 
-// 文字単位の処理(camel_to_snake用)
-fn to_lowercase(c: char) -> String {
+/// Convert camel case to kebab case
+/// # Example
+/// ```
+/// use case_converter::camel_to_kebab;
+///
+/// let result = camel_to_kebab("camelToKebab");
+///  println!("{}", result); // → camel-to-kebab
+/// ```
+pub fn camel_to_kebab(arg_str: &str) -> String {
+    let chars: Vec<char> = arg_str.chars().collect();
+    chars.into_iter()
+        .map(|c| to_lowercase(c, '-'))
+        .collect()
+}
+
+// 文字単位の小文字変換処理
+fn to_lowercase(c: char, s: char) -> String {
     if c.is_uppercase() {
         let mut chars: Vec<char> = Vec::new();
-        chars.push('_');
+        chars.push(s);
         chars.push(c.to_ascii_lowercase());
         String::from_iter(chars)
     } else {
         c.to_string()
     }
+}
+
+/// Convert kebab case to snake case
+/// # Example
+/// ```
+/// use case_converter::kebab_to_snake;
+///
+/// let result = kebab_to_snake("kebab-to-snake");
+///  println!("{}", result); // → kebab_to_snake
+/// ```
+pub fn kebab_to_snake(arg_str: &str) -> String {
+    arg_str.replace("-", "_")
+}
+
+/// Convert snake case to kebab case
+/// # Example
+/// ```
+/// use case_converter::snake_to_kebab;
+///
+/// let result = snake_to_kebab("snake_to_kebab");
+///  println!("{}", result); // → snake-to-kebab
+/// ```
+pub fn snake_to_kebab(arg_str: &str) -> String {
+    arg_str.replace("_", "-")
 }
 
 #[cfg(test)]
@@ -82,9 +141,37 @@ mod tests {
     }
 
     #[test]
+    fn test_kebab_to_camel() {
+        assert_eq!(kebab_to_camel("kebab"), "kebab");
+        assert_eq!(kebab_to_camel("kebab-to"), "kebabTo");
+        assert_eq!(kebab_to_camel("kebab-to-camel"), "kebabToCamel");
+    }
+
+    #[test]
     fn test_camel_to_snake() {
         assert_eq!(camel_to_snake("camel"), "camel");
         assert_eq!(camel_to_snake("camelTo"), "camel_to");
         assert_eq!(camel_to_snake("camelToSnake"), "camel_to_snake");
+    }
+
+    #[test]
+    fn test_camel_to_kebab() {
+        assert_eq!(camel_to_kebab("camel"), "camel");
+        assert_eq!(camel_to_kebab("camelTo"), "camel-to");
+        assert_eq!(camel_to_kebab("camelToKebab"), "camel-to-kebab");
+    }
+
+    #[test]
+    fn test_kebab_to_snake() {
+        assert_eq!(kebab_to_snake("kebab"), "kebab");
+        assert_eq!(kebab_to_snake("kebab-to"), "kebab_to");
+        assert_eq!(kebab_to_snake("kebab-to-snake"), "kebab_to_snake");
+    }
+
+    #[test]
+    fn test_snake_to_kebab() {
+        assert_eq!(snake_to_kebab("kebab"), "kebab");
+        assert_eq!(snake_to_kebab("kebab_to"), "kebab-to");
+        assert_eq!(snake_to_kebab("snake_to_kebab"), "snake-to-kebab");
     }
 }
